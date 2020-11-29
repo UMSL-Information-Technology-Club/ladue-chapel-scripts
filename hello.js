60f6382c8d7206269efe740ae886367655ff8a53
Nico Stranquist <nstranquist@gmail.com>
2020-11-28 21:10:16 -0600

    remove unused files

--- hello.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-  Logger.log("hello")
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheets()[0];
-var range = sheet.getRange('A2:W2');
-  console.log(range.toString())
-  for(item in range) {
-    console.log(item)
-  }
-}
--- test-script.js
+++
@@ -1,7 +0,0 @@
-// testing local uploads
-// Hello world :)
-function myFunction () {
-  console.log('hello')
-  // changes
-  console.log('ive been changed')
-}
